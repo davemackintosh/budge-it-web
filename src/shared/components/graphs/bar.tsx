5447774085adb02cd16b3d42f60979add9b5bd17
davemackintosh <me@davemackintosh.co.uk>
2019-09-29 21:48:19 +0100

    Working on bar graph

--- src/shared/components/graphs/bar.tsx
+++ src/shared/components/graphs/bar.tsx
@@ -1,7 +1,8 @@
-import React, { Fragment, useContext } from "react"
+import React, { Fragment, useContext, useState, SyntheticEvent } from "react"
 import { BarGraphBar } from "@src/shared/theme/graphs/bar"
 import { CSVContext } from "@src/shared/contexts/csv"
-import { normalise } from "@src/shared/utils"
+import { normalise, monthNames } from "@src/shared/utils"
+import { ParsedCsvEntry } from "types/csv"
 
 export interface BarGraphPropsData {
   x: number | Date
@@ -14,34 +15,57 @@ interface Props {
   svgHeight: number
 }
 
+type BarData = Record<number, Record<string, number>>
+
 const BarGraph = (props: Props): JSX.Element => {
+  const [currentYear, updateCurrentYear] = useState<number>(
+    new Date().getFullYear(),
+  )
   const csvContext = useContext(CSVContext)
-  const data = {}
-  const spansMultipleYears = false
-  const { minY, maxY } = csvContext
+  const years = csvContext.parsedCsvFile.reduce(
+    (acc: BarData, entry: ParsedCsvEntry): BarData => {
+      const date = new Date(entry.date)
+      const year = date.getFullYear()
+      const month = monthNames[date.getMonth()]
+      console.log(year, month)
+
+      if (!acc.hasOwnProperty(year)) acc[year] = {}
+
+      if (!acc[year].hasOwnProperty(month)) acc[year][month] = 0
+
+      acc[year][month] += entry.difference
+
+      return acc
+    },
+    {},
+  )
+
   const margin = 5
-  const width =
-    props.svgWidth -
-    (margin * csvContext.parsedCsvFile.length) / csvContext.parsedCsvFile.length
+  const width = props.svgWidth - (margin * 12) / 12
+  const minY = Math.min(...Object.values(years[currentYear]))
+  const maxY = Math.max(...Object.values(years[currentYear]))
+  const onYearChange = (event: SyntheticEvent<HTMLSelectElement>): void =>
+    updateCurrentYear(Number(event.currentTarget.value))
 
   let yearSelector = null
 
-  if (spansMultipleYears)
+  if (Object.keys(years).length > 1) {
     yearSelector = (
-      <select name="year">
-        {Object.keys(data).map(year => (
+      <select onChange={onYearChange} name="year" value={currentYear}>
+        {Object.keys(years).map(year => (
           <option key={year} value={year}>
             {year}
           </option>
         ))}
       </select>
     )
+  }
 
   return (
     <Fragment>
       {yearSelector}
-      {data.map((row, index) => {
-        const height = props.svgHeight * normalise(row.y, minY, maxY)
+      {Object.values(years[currentYear]).map((amount, index) => {
+        const height = props.svgHeight * normalise(amount, minY, maxY)
 
         return (
           <BarGraphBar
